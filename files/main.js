4616d982a82a6598fd1802df250c59c1cba02aab
AzAlex <99219332+azalex020204@users.noreply.github.com>
2022-08-02 20:34:18 +0300

    Add files via upload

---
+++ files/main.js
@@ -0,0 +1,34 @@
+$(function () {
+  $('.sliders').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },]
+  });
+  $('.sliders2').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },]
+  });
+})
